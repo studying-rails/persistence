b630292b3b4071f1638249dd8cc6b0dbcb64deb6
OTA Daichi <daichi.ota@access-company.com>
2013-07-19 09:29:00 +0900

    Create project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Persistence::Application.config.secret_token = 'dd0afe4372dfe615a4f57e9c71d9610b4105a2888ce4ed7e3abbc480ff373ecab86574d858e7bb0ea588684e445f4578323dd92ebcaf01da4a81bda186e51039'
